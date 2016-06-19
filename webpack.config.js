b004548d097277cd2f35c7ff9037c74f4f82c7cd
Dafrok <o.o@mug.dog>
2016-06-19 10:36:23 +0800

    add standard ignore

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: 'app.js'
   },
   resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
+    root: path.join(__dirname, 'node_modules')
   },
   module: {
     loaders: [
