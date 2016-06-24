96a472a71d073882326b1610244192a1c5a793d4
Dafrok <o.o@mug.dog>
2016-06-24 18:35:44 +0800

    Modal

--- src/components/app.vue
+++ src/components/app.vue
@@ -2,13 +2,16 @@
 div#app
   site-header
   site-footer
+  modal(show)
 </template>
 
 <script>
 import SiteHeader from './util/site-header.vue'
 import SiteFooter from './util/site-footer.vue'
+import Modal from './util/modal.vue'
 export default {
   components: {
+    modal: Modal,
     siteHeader: SiteHeader,
     SiteFooter: SiteFooter
   }
---
+++ src/components/util/modal.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div.modal(v-bind:class="{'fade': !show}")
+  div.modal-dialog(role="document")
+    div.modal-content
+      div.modal-header
+        button.close(type="button", data-dismiss="modal", aria-label="Close")
+          span(aria-hidden="true") &times;
+        h4.modal-title Modal title
+      div.modal-body
+        p One fine body&hellip;
+      div.modal-footer
+        button.btn.btn-secondary(type="button", data-dismiss="modal") Close
+        button.btn.btn-primary(type="button") Save changes
+</template>
+<script>
+export default {
+  props: ['show']
+}
+</script>
+<style lang="stylus">
+.modal
+  display block
+</style>
